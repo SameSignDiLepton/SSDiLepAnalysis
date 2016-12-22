a8d90b821d6e90218720f36fee291ce82f17e694
MihaMuskinja <miha.muskinja@gmail.com>
2016-12-22 16:42:50 +0100

    made the electronEffCorr config more compact and added SFs for all the 3 trigger combos. However, only LHLoose and LHMedium+looseIso are now supported

--- scripts/jobOptions_SSDiLep_v2.py
+++ scripts/jobOptions_SSDiLep_v2.py
@@ -7,6 +7,29 @@ sys.path.insert(0, os.environ['ROOTCOREBIN']+"/user_scripts/SSDiLepAnalysis/")
 
 from config_v2_182ifb import *
 
+def generateElectronEfficiencyCorrector (path, PID, isol, trigger) :
+  ElectronEfficiencyCorrector = { "m_name" : "EleEffCorr"+PID+isol+trigger,
+  "m_debug"                 : False,
+  "m_inContainerName"       : "Electrons_OR",
+  "m_inputAlgoSystNames"    : "ElectronSelector_Syst",
+  "m_systNameReco"          : "",
+  "m_systNameIso"           : "",
+  "m_systNamePID"           : "",
+  "m_systNameTrig"          : "",
+  "m_systNameTrigMCEff"     : "",
+  "m_outputSystNamesReco"   : "EleEffCorr_RecoSyst",
+  "m_outputSystNamesPID"    : "EleEffCorr_PIDSyst" ,
+  "m_outputSystNamesIso"    : "EleEffCorr_IsoSyst" ,
+  "m_outputSystNamesTrig"   : "EleEffCorr_TrigSyst",
+  "m_outputSystNamesTrigMCEff"   : "EleEffCorr_TrigMCEffSyst",
+  "m_corrFileNameReco"      : path + "offline/efficiencySF.offline.RecoTrk.root",
+  "m_corrFileNamePID"       : path + "offline/efficiencySF.offline."         + PID + "_d0z0_v11.root" if len(PID)>0 else "",
+  "m_corrFileNameIso"       : path + "isolation/efficiencySF.Isolation."     + PID + "_d0z0_v11" + isol + ".root" if len(isol)>0 else "",
+  "m_corrFileNameTrig"      : path + "trigger/efficiencySF." + trigger + "." + PID + "_d0z0_v11" + isol + ".root" if len(trigger)>0 else "",
+  "m_corrFileNameTrigMCEff" : path + "trigger/efficiency."   + trigger + "." + PID + "_d0z0_v11" + isol + ".root" if len(trigger)>0 else "",
+  }
+  return ElectronEfficiencyCorrector
+
 c = xAH_config()
 
 # Here order matters!
@@ -37,21 +60,31 @@ c.setalg("MuonEfficiencyCorrector", MuonEfficiencyCorrectorMediumGradientLooseDi
 # --------------------
 # electron corrections
 # --------------------
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorLooseDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorLooseFCLDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorLooseLTODict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorMediumDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorMediumGradientDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorMediumLooseDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorMediumLTODict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorTightDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorTightGradientDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorTightLooseDict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorTightLTODict)
-c.setalg("ElectronEfficiencyCorrector", ElectronEfficiencyCorrectorTightTightDict)
 
+# save scale-factors only for two combinations of ID/isol WPs:
+#  -) LooseAndBLayerLLH + no iso
+#  -) MediumLLH         + isolLoose
+
+path_el_eff = "ElectronEfficiencyCorrection/2015_2016/rel20.7/ICHEP_June2016_v3/"
+
+el_IDWPs   = ["LooseAndBLayerLLH","MediumLLH" ]
+el_isolWPs = [""                 ,"_isolLoose"]
+
+trigger_el_single               = "SINGLE_E_2015_e24_lhmedium_L1EM20VH_OR_e60_lhmedium_OR_e120_lhloose_2016_e26_lhtight_nod0_ivarloose_OR_e60_lhmedium_nod0_OR_e140_lhloose_nod0"
+trigger_el_double_unrecommended = "DI_E_2015_e17_lhloose_2016_e17_lhloose"
+trigger_el_double_recommended   = "DI_E_2015_e12_lhloose_L1EM10VH_2016_e17_lhvloose_nod0"
+
+el_trigWPs = [trigger_el_single,trigger_el_double_unrecommended,trigger_el_double_recommended]
+
+for ID,isol in zip(el_IDWPs,el_isolWPs):
+  for trig in el_trigWPs:
+    c.setalg("ElectronEfficiencyCorrector", generateElectronEfficiencyCorrector(path_el_eff,ID,isol,trig) )
+c.setalg("ElectronEfficiencyCorrector", generateElectronEfficiencyCorrector(path_el_eff,"TightLLH","","") )
+c.setalg("ElectronEfficiencyCorrector", generateElectronEfficiencyCorrector(path_el_eff,"LooseAndBLayerLLH","_isolLoose","") )
+
+SSDiLepTreeAlgoDict["m_elDetailStr"] = trigger_el_single + " " + trigger_el_double_unrecommended + " " + trigger_el_double_recommended \
+                                     + " LooseAndBLayerLLH MediumLLH TightLLH isolNoRequirement isolLoose kinematic trigger isolation PID trackparams effSF"
 
 c.setalg("TruthMatchAlgo", TruthMatchAlgoDict)
 c.setalg("XSAlgo", XSAlgoDict)
 c.setalg("SSDiLepTreeAlgo", SSDiLepTreeAlgoDict)
-
