c282f109cb75ebd42ba5138577172e0e31d53eac
MihaMuskinja <miha.muskinja@gmail.com>
2017-01-23 10:39:13 +0100

    updated dataset files

--- scripts/gridlaunch_SSDiLep.sh
+++ scripts/gridlaunch_SSDiLep.sh
@@ -10,7 +10,7 @@ username=mmuskinj
 #prodtag=presc
 #prodtag=allsingle
 
-prodtag=v2_EXOT12_data
+prodtag=v2_EXOT12_MC
 
 ##infilepath="/home/fscutti/Analysis/SSDiLepAnalysis/doc/grid_input.txt"
 ##infilepath="/home/fscutti/Analysis/SSDiLepAnalysis/doc/single_grid_input.txt"
@@ -28,7 +28,18 @@ prodtag=v2_EXOT12_data
 ## infilepath="/home/fscutti/Analysis/SSDiLepAnalysis/doc/data15_13TeV_physics_Main_DAOD_EXOT12_p2528.txt"
 
 #infilepath="/home/fscutti/Analysis/SSDiLepAnalysis/doc/data_bkg_sig.txt"
-infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT12Data.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT12Data.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT12Data_failed.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT19Data.txt"
+# infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT19Data_failed.txt"
+infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT12MC_higgs.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT19Data2015.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT0test.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT0.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/MCEXOT12signal.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/MCEXOT12signal2.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/EXOT12Data2015.txt"
+#infilepath="/afs/cern.ch/work/g/gorisek/miha/submitV2NtuplesMiha/SSDiLepAnalysis/doc/MCEXOT12.txt"
 
 
 configpath="$ROOTCOREBIN/user_scripts/SSDiLepAnalysis/jobOptions_SSDiLep_v2.py"
@@ -38,7 +49,7 @@ outdir=output_grid_DxAOD-2015-13TeV_${current_time}
 
 #inSE=INFN-T1_DATADISK,MWT2_DATADISK
 
-destSE=AUSTRALIA-ATLAS_LOCALGROUPDISK
+destSE=SIGNET_LOCALGROUPDISK
 #exclSE=ANALY_IHEP,ANALY_JINR,ANALY_IHEP_GLEXEC,ANALY_RRC-KI-HPC,ANALY_RRC-KI-T1,IHEP_MCORE,IHEP_PROD,RRC-KI-HPC2,RRC-KI-T1,RRC-KI-T1_MCORE,RRC-KI-T1_TEST,ANALY_MWT2_SL6,ANALY_PIC_SL6,ANALY_TRIUMF,ANALY_INFN-ROMA1,ANALY_INFN-NAPOLI,ANALY_BNL_SHORT,ANALY_BNL_LONG
 
 #exclSE=ANALY_INFN-FRASCATI,ANALY_FREIBURG,ANALY_FZK,ANALY_FZK_HI,ANALY_FZK_SHORT,ANALY_LANCS_SL6,ANALY_ARNES,ANALY_GRIF-LPNHE,ANALY_IFIC,ANALY_NSC,ANALY_QMUL_HIMEM_SL6,ANALY_QMUL_SL6,ANALY_SiGNET,ANALY_UAM,ANALY_UIO   
@@ -46,7 +57,7 @@ destSE=AUSTRALIA-ATLAS_LOCALGROUPDISK
 ##exclSE=ANALY_INFN-T1,ANALY_MWT2_SL6,ANALY_OU_OCHEP_SWT2,ANALY_SWT2_CPB,ANALY_BU_ATLAS_Tier2_SL6,ANALY_HU_ATLAS_Tier2,ANALY_PIC_SL6,ANALY_MANC_SL6,ANALY_OX_SL6,ANALY_DCSC,ANALY_LUNARC,ANALY_SCINET,ANALY_SLAC,RU-Protvino-IHEP
 
 
-exclSE=OUHEP_OSG,INFN-ROMA2,ru-Moscow-FIAN-LCG2,UNI-DORTMUND,RRC-KI-T1,OU_OCHEP_SWT2,SFU-LCG2,PSNC,UKI-SCOTGRID-GLASGOW,UNI-FREIBURG,UNIBE-LHEP,HEPHY-UIBK,INFN-FRASCATI,EELA-UTFSM,BEIJING-LCG2,SMU_HPC,TUDresden-ZIH,SARA-MATRIX,UKI-LT2-RHUL
+exclSE=FZK-LCG2,Microsoft-Azure,UKI-SCOTGRID-ECDF,RAL-LCG2,SMU_HPC,T2_PK_NCP,T2_FI_HIP,CA-SCINET-T2,Indiana,ru-Moscow-FIAN-LCG2,IEPSAS-Kosice,OU_OCHEP_SWT2,UNI-SIEGEN-HEP,CERN-EXTENSION,CSCS-LCG2,T1_UK_RAL,UKI-SOUTHGRID-RALPP,OUHEP_OSG,DESY-HH,T2_GR_Ioannina,T2_IN_TIFR,CA-VICTORIA-WESTGRID-T2,UAM-LCG2,RAL-LCG2-ECHO,UKI-SCOTGRID-ECDF-RDF,T1_DE_KIT,CERN-CMSTEST,CERN-PROD,CERN-P1,IAAS,IN2P3-CPPM
 
 ##exclSE=RU-Protvino-IHEP
 
@@ -54,7 +65,9 @@ gridDSname="user.${username}.SSDiLep.${prodtag}.%in:name[2]%.%in:name[3]%"
 
 #xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputDQ2 --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridDestSE=${destSE} --optGridOutputSampleName=${gridDSname} --optGridExcludedSite=${exclSE} #--optGridSite=${inSE} 
 
-xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputDQ2 --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname} --optGridExcludedSite=${exclSE} 
+#xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputRucio --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname} --optGridExcludedSite=${exclSE} --optGridDestSE=${destSE} 
+xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputRucio --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname} --optGridExcludedSite=${exclSE}
+#xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputRucio --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname}
 
 
 
--- scripts/launch_SSDiLep.sh
+++ scripts/launch_SSDiLep.sh
@@ -1,33 +1,7 @@
 #!/bin/bash
 
+infilepath = "/afs/cern.ch/work/g/gorisek/miha/mc15_13TeV.410000.PowhegPythiaEvtGen_P2012_ttbar_hdamp172p5_nonallhad.merge.DAOD_EXOT12.e3698_s2608_s2183_r7725_r7676_p2823/DAOD_EXOT12.09520689._000053.pool.root.1"
 
-#inDS="data16_13TeV.00304494.physics_Main.merge.DAOD_EXOT12.f716_m1620_p2840"
-#inDS="data16_13TeV.00304494.physics_Main.merge.DAOD_EXOT19.f716_m1620_p2840"
-#inDS="mc15_13TeV.301003.PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYee_400M600.merge.DAOD_EXOT0.e3649_s2576_s2132_r7772_r7676_p2669"
-#inDS="mc15_13TeV.301006.PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYee_1000M1250.merge.DAOD_EXOT12.e3649_s2576_s2132_r7772_r7676_p2823"
-
-#inDS="data15_13TeV.00284484.physics_Main.merge.DAOD_EXOT19.r7562_p2521_p2840"
-#inDS="testRTT"
-#inDS="mc15_13TeV"
-#inDS="mc15_13TeV.301012.PowhegPythia8EvtGen_AZNLOCTEQ6L1_DYee_2500M2750.merge.DAOD_EXOT0.e3649_s2576_s2132_r7772_r7676_p2669"
-inDS="mc15_13TeV.361106.PowhegPythia8EvtGen_AZNLOCTEQ6L1_Zee.merge.DAOD_EXOT12.e3601_s2576_s2132_r7725_r7676_p2823"
-
-#sample="DAOD_EXOT12.09582252._000005.pool.root.1"
-#sample="DAOD_EXOT19.09582252._000010.pool.root.1"
-#sample="DAOD_EXOT0.08616868._000001.pool.root.1"
-#sample="DAOD_EXOT12.09520314._000001.pool.root.1"
-
-#sample="DAOD_EXOT19.09581433._000066.pool.root.1"
-#sample="DAOD_EXOT12.MC15.pool.root"
-#sample="DAOD_EXOT12.Data16.pool.root"
-#sample="DAOD_EXOT12.09777905._000001.pool.root.1"
-#sample="DAOD_EXOT0.08617028._000001.pool.root.1"
-sample="DAOD_EXOT12.09520232._000002.pool.root.1"
-
-#infilepath="/afs/cern.ch/work/g/gkramber/miha/testDxAOD/${inDS}/${sample}"
-infilepath="/afs/cern.ch/work/g/gorisek/miha/${inDS}/${sample}"
-#infilepath="/afs/cern.ch/work/g/gkramber/miha/testDxAOD/${inDS}/${sample}"
-#infilepath="/afs/cern.ch/work/g/gorisek/miha/CutflowChallenge/${inDS}/${sample}"
 
 # ------------------------------------------------------------------------------------
 
@@ -37,7 +11,7 @@ tokens=(${inDS//./ })
 configpath="$ROOTCOREBIN/user_scripts/SSDiLepAnalysis/jobOptions_SSDiLep_v2.py"
 current_time="$(date +'%d-%m-%Y-%T')"
 outdir=output_local_DxAOD-2015-13TeV_${tokens[2]}_${current_time}
-nevents=200
+nevents=500
 
 echo ""
 echo "Input file path :"
