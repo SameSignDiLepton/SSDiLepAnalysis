ec0901722eb347cfbd8b266bb4990ba4ae5b588c
Federico <federicoscutti@gmail.com>
2017-02-04 22:32:57 +0000

    di-muon channel config update

--- scripts/gridlaunch_SSDiMu.sh
+++ scripts/gridlaunch_SSDiMu.sh
@@ -5,23 +5,25 @@
 
 username=fscutti
 
-prodtag=HIGG3D3_v8
+#prodtag=HIGG3D3_v8
+prodtag=EXOT12_pv8
 
-infilepath="/home/fscutti/SSDiLepAnalysis/doc/all_v8_HIGG3D3.txt"
+#infilepath="/home/fscutti/SSDiLepAnalysis/doc/all_pv2_EXOT12.txt"
+#infilepath="/home/fscutti/SSDiLepAnalysis/doc/all_v8_HIGG3D3.txt"
 #infilepath="/home/fscutti/SSDiLepAnalysis/doc/jetjet_HIGG3D3.txt"
 
-#infilepath="/home/fscutti/SSDiLepAnalysis/doc/failed_jobs.txt"
+infilepath="/home/fscutti/SSDiLepAnalysis/doc/failed_jobs.txt"
 
 configpath="$ROOTCOREBIN/user_scripts/SSDiLepAnalysis/jobOptions_SSDiMu.py"
 
 current_time="$(date +'%d-%m-%Y-%T')"
 outdir=output_grid_DxAOD-2016-13TeV_${current_time}
 
-exclSE=OUHEP_OSG,OU_OCHEP_SWT2,ru-Moscow-FIAN-LCG2,INFN-T1,CSCS-LCG2,HEPHY-UIBK
+exclSE=OUHEP_OSG,OU_OCHEP_SWT2,ru-Moscow-FIAN-LCG2,CSCS-LCG2,UKI-SOUTHGRID-RALPP
 
 gridDSname="user.${username}.SSDiLep.${prodtag}.%in:name[2]%.%in:name[3]%"
 
-xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputDQ2 --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname} ##--optGridExcludedSite=${exclSE} 
+xAH_run.py -vv --files ${infilepath} --config ${configpath} --inputList --inputDQ2 --submitDir ${outdir} prun --optGridMergeOutput=1 --optGridNFilesPerJob=1.0 --optGridOutputSampleName=${gridDSname} #--optGridExcludedSite=${exclSE} 
 
 
 
